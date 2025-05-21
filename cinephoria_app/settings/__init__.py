e955cc00017425c1c70f52f21e386aad7422b520
wassila-ben <wassilabouhadji@hotmail.fr>
2025-05-21 18:44:17 +0200

    force prod.py

--- cinephoria_app/settings/__init__.py
+++ cinephoria_app/settings/__init__.py
@@ -1,8 +1,2 @@
-import os
+from .prod import *
 
-ENV= os.environ.get('DJANGO_ENV', 'dev') # Valeur par défaut 'dev' si la variable d'environnement n'est pas définie
-
-if ENV == 'prod':
-    from .prod import *
-else : 
-    from .dev import *
